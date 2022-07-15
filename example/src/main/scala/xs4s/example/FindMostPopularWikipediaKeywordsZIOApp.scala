c1b8c3437fe9fddeade9c7af2bcdb47ca5479039
Landlocked Surfer <landlocked.surfer@yahoo.com>
2022-07-15 23:43:53 +0200

    Upgrade to ZIO 2

--- example/src/main/scala/xs4s/example/FindMostPopularWikipediaKeywordsZIOApp.scala
+++ example/src/main/scala/xs4s/example/FindMostPopularWikipediaKeywordsZIOApp.scala
@@ -1,46 +1,35 @@
 package xs4s.example
 
 import zio._
-import zio.console._
 import zio.stream._
-import zio.blocking.Blocking
-import java.io.IOException
-import java.io.InputStream
-import java.net.URL
-import java.net.URLConnection
-import java.util.zip.GZIPInputStream
-import scala.collection.JavaConverters._
+
+import java.io.{IOException}
 import xs4s.ziocompat._
 import xs4s.syntax.zio._
 
-object FindMostPopularWikipediaKeywordsZIOApp extends zio.App {
+object FindMostPopularWikipediaKeywordsZIOApp extends zio.ZIOAppDefault {
 
-  def run(args: List[String]): URIO[ZEnv, ExitCode] = {
+  override def run: ZIO[ZIOAppArgs with Scope, Any, Any] = {
 
-    wikipediaXmlBytes
-      .via(byteStream => byteStreamToXmlEventStream()(byteStream))
-      .via(anchorExtractor.toZIOPipeThrowError(_))
-      .via(stream => // by default, get a sub-set of the stream
-        if (args.contains("full")) stream else stream.take(500)
-      )
-      .map(_.text)
-      .via(countTopItemsZIO(_))
-      .flatMap(counts => ZStream.fromIterable(counts))
-      .foreach{ case (elem: String, count: Int) => putStrLn((count, elem).toString) }
-      .as(ExitCode.success)
-      .orDie
+    getArgs.flatMap { args =>
+      wikipediaXmlBytes
+        .viaFunction((byteStream: ZStream[Scope, Throwable, Byte]) => byteStreamToXmlEventStream()(byteStream))
+        .viaFunction(anchorExtractor.toZIOPipeThrowError(_))
+        .viaFunction(stream => // by default, get a sub-set of the stream
+          if (args.contains("full")) stream else stream.take(500)
+        )
+        .map(_.text)
+        .viaFunction(countTopItemsZIO(_))
+        .flatMap(counts => ZStream.fromIterable(counts))
+        .foreach { case (elem: String, count: Int) => zio.Console.printLine((count, elem).toString) }
+        .orDie
+    }
   }
 
   /**
     * Can be any of your own XML byte stream
-    */  
-  private val wikipediaXmlBytes: ZStream[Blocking, IOException, Byte] = {
-    val is: ZIO[Blocking, IOException, InputStream] = blocking.effectBlockingIO {
-      // TODO: Use ZIO gzip transducer after it ships:
-      //   https://github.com/zio/zio/issues/3759
-      new GZIPInputStream(wikipediaAbstractURL.openStream())
-    }
-
-    Stream.fromInputStreamEffect(is, 1024)
+    */
+  private val wikipediaXmlBytes: ZStream[Scope, IOException, Byte] = {
+    ZStream.fromInputStream(wikipediaAbstractURL.openStream(),1024).via(ZPipeline.gunzip().orDie)
   }
 }
--- example/src/main/scala/xs4s/example/brief/BriefZIOExample.scala
+++ example/src/main/scala/xs4s/example/brief/BriefZIOExample.scala
@@ -1,7 +1,6 @@
 package xs4s.example.brief
 
 import java.io.IOException
-import zio.blocking.Blocking
 
 /** An example that compiles but does not run - for use in the README */
 object BriefZIOExample {
@@ -19,18 +18,18 @@ object BriefZIOExample {
     * @param byteStream Could be, for example, zio.stream.Stream.fromInputStream(inputStream)
     * @return
     */
-  def extractAnchorTexts[R <: Blocking](byteStream: ZStream[R, IOException, Byte]): ZStream[R, Throwable, String] = {
+  def extractAnchorTexts[R](byteStream: ZStream[R, IOException, Byte]): ZStream[R with Scope, Throwable, String] = {
     /** extract all elements called 'anchor' **/
     val anchorElementExtractor: XmlElementExtractor[Elem] =
       XmlElementExtractor.filterElementsByName("anchor")
 
     /** Turn into XMLEvent */
-    val xmlEventStream: ZStream[R, Throwable, XMLEvent] =
-      byteStream.via(byteStreamToXmlEventStream()(_))
+    val xmlEventStream: ZStream[R with Scope, Throwable, XMLEvent] =
+      byteStream.viaFunction(byteStreamToXmlEventStream[R]()(_))
 
     /** Collect all the anchors as [[scala.xml.Elem]] */
-    val anchorElements: ZStream[R, Throwable, Elem] =
-      xmlEventStream.via(anchorElementExtractor.toZIOPipeThrowError)
+    val anchorElements: ZStream[R with Scope, Throwable, Elem] =
+      xmlEventStream.viaFunction(anchorElementExtractor.toZIOPipeThrowError)
 
     /** And finally extract the text contents for each Elem */
     anchorElements.map(_.text)
